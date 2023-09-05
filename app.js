b3f281765bb843e987d12c174724a9446d739c16
nadud99 <wawa9149@naver.com>
2023-09-05 14:13:29 +0900

    feat

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+
+//메뉴 탭 선택 시 색상 변경
+const menuWrap = document.querySelector('#menuList');
+
+function movePage(a){
+    window.scrollTo({ top: a, behavior: "smooth" });
+}
+
+function select(ulEl, liEl){
+    Array.from(ulEl.children).forEach(
+        v => v.classList.remove('selected')
+    )
+    if(liEl.innerText === 'Profile') {
+        liEl.classList.add('selected');
+        movePage(330);
+    }
+    else if(liEl.innerText === 'Skills'){
+        liEl.classList.add('selected');
+        movePage(1080);
+    }
+    else if(liEl.innerText === 'Project'){
+        liEl.classList.add('selected');
+        movePage(1700);
+    }
+}
+
+menuWrap.addEventListener('click', e => {
+    const selected = e.target;
+    select(menuWrap, selected);
+})
+
+//jQuery, 메뉴 바 스크롤 시 고정
+$(document).ready(function(){
+ 
+    var topMenu = $("#menu").offset();
+ 
+    $(window).scroll(function(){
+        
+        var docScrollY = $(document).scrollTop()
+        var barThis = $("#menu")
+        var fixNext = $(".main-content")
+ 
+        if( docScrollY > topMenu.top ) {
+            barThis.addClass("top_bar_fix");
+            fixNext.addClass("pd_top_80");
+        }else{
+            barThis.removeClass("top_bar_fix");
+            fixNext.removeClass("pd_top_80");
+        }
+ 
+    });
+ 
+})
+
+//night&day mode
 var Body = {
     setColor:function (color){
         //document.querySelector('body').style.color = color;
